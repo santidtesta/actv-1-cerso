db96044b9188931155f729b176ef5bfe35622d0e
Informatica 7º <info7@almafuerte.edu>
2023-03-28 20:04:53 -0300

    actividad finalizada

---
+++ src/hola/Hola.java
@@ -0,0 +1,12 @@
+package hola;
+/**
+ * La clase hola construye un programa que visualiza el mensaje
+ * "hola, me llamo tu_nombre" en el dispositivo de salida estandar
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+      
+
+    }
+}
